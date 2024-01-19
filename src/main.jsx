001d8bbcf5b3625c462a23153938c0695b4a52e3
carlosMG <guzcar888@gmail.com>
2024-01-19 16:35:11 -0600

    Inicio del proyecto

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Primero } from './primero'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Primero titulo="wasaa"/>
+  </React.StrictMode>,
+)
---
+++ src/primero.jsx
@@ -0,0 +1,9 @@
+
+export const Primero = ({titulo = 'Esto es otro titulo'}) => {
+  return (
+    <>
+      <h1>{titulo}</h1>
+    </>
+  )
+}
+
